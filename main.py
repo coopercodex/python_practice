6e21ad8d9685059fa456142bad20e904dd6f8568
Derek Cooper <derekcooper20@gmail.com>
2023-04-11 15:15:53 -0600

    ex2

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-print("Welcome to the tip calculator")
+# print("Welcome to the tip calculator")
 
-total = float(input("What was the total bill? $"))
+# total = float(input("What was the total bill? $"))
 
-tip = int(input(f"What percentage tip would you like to give? 20, 22? Or 25?  "))
+# tip = int(input(f"What percentage tip would you like to give? 20, 22? Or 25?  "))
 
-group_size = int(input("How many people will split the bill? "))
+# group_size = int(input("How many people will split the bill? "))
 
-bill_with_tip = tip / 100 * total + total
+# bill_with_tip = tip / 100 * total + total
 
-amount = bill_with_tip / group_size
+# amount = bill_with_tip / group_size
 
-final_amount ="{:.2f}".format(amount)
+# final_amount ="{:.2f}".format(amount)
 
-print(f"Each person should pay ${final_amount}")
+# print(f"Each person should pay ${final_amount}")
+
+
+
+#1. Create a greeting for your program.
+print("Hello welcome to Band Generator")
+
+#2. Ask the user for the city that they grew up in.
+pet_name = input("What is your pets name?\n")
+#3. Ask the user for the name of a pet.
+user_city = input("What city did you grow up in?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your bands name is " + pet_name + " " + user_city + "\n")
